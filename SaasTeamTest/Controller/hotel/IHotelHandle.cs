1df2b112213bba1c2d929316e0d73656a46d6621
Rhican Ron <Rhican.R@gmail.com>
2022-07-24 20:38:58 +0800

    add hotel handle interface contract

---
+++ SaasTeamTest/Controller/hotel/IHotelHandle.cs
@@ -0,0 +1,16 @@
+﻿using SaasTeamTest.Models;
+using System.Collections.Generic;
+
+namespace SaasTeamTest.Controller.hotel
+{
+    public interface IHotelHandle
+    {
+        public Room QuickCheckIn();
+        public bool Checkout(Room room);
+        public bool Clean(Room room);
+        public bool RequestRepair(Room room);
+        public IList<Room> AvailableRooms();
+
+        public bool CompleteRepairing(Room room);
+    }
+}
