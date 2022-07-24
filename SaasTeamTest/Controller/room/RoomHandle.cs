9d6173eab485f18323840469c68dffdeb13f5ab4
Rhican Ron <Rhican.R@gmail.com>
2022-07-24 20:39:25 +0800

    Add Room Handle Base

---
+++ SaasTeamTest/Controller/room/RoomHandle.cs
@@ -0,0 +1,18 @@
+﻿using SaasTeamTest.Models;
+
+namespace SaasTeamTest.Controller
+{
+    abstract class RoomHandle 
+    {
+        public RoomHandle(Room room)
+        {
+            Room = room;
+        }
+        public Room Room { get; private set; }
+
+        public virtual bool CanAccess(UserRole role)
+        {
+            return false;
+        }
+    }
+}
